9af7bc48870540af916731223b2c446efa2c9767
jgaviria-at-514287058315 <0WwJBox0e/NOjrJV/Cr3P0S4b913zkz98yqbtyj6CBQ=>
2017-12-11 07:23:16 -0500

    JG: Siembra inicial del proyecto

---
+++ src/main/java/com/altamontana/AltamontanaApplication.java
@@ -0,0 +1,12 @@
+package com.altamontana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltamontanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltamontanaApplication.class, args);
+	}
+}
