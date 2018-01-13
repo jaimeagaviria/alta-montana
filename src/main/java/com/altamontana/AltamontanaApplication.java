50029f7c3f74d3878c21a1860f3eec363408f12e
jgaviria-at-514287058315 <0WwJBox0e/NOjrJV/Cr3P0S4b913zkz98yqbtyj6CBQ=>
2018-01-13 08:24:46 -0500

    JG: Siembra inicial del proyecto

--- src/main/java/com/altamontana/AltamontanaApplication.java
+++ src/main/java/com/altamontana/AltamontanaApplication.java
@@ -1,9 +1,22 @@
 package com.altamontana;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.altamontana.*")
+@EnableCaching
+@EnableScheduling
+@EnableAsync
 public class AltamontanaApplication {
 
 	public static void main(String[] args) {
