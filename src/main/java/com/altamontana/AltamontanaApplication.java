d1f1aa0fb2cd1f7623265e13272e91ecf8d2035d
jgaviria-at-514287058315 <0WwJBox0e/NOjrJV/Cr3P0S4b913zkz98yqbtyj6CBQ=>
2018-04-24 13:49:36 -0500

    JG: Se ocultan las recetas en el reporte al momento de imprimir

--- src/main/java/com/altamontana/AltamontanaApplication.java
+++ src/main/java/com/altamontana/AltamontanaApplication.java
@@ -4,13 +4,21 @@ import com.altamontana.repository.impl.AbstractRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
 @Configuration
@@ -26,4 +34,19 @@ public class AltamontanaApplication extends SpringBootServletInitializer{
 		SpringApplication.run(AltamontanaApplication.class, args);
 	}
 
+	/***
+	 * Este bean se implementa para poder atnder las peticiones Angular y evitar mostrar el mensaje de error 404
+	 * @return
+	 */
+	@Bean
+	ErrorViewResolver supportPathBasedLocationStrategyWithoutHashes() {
+		return new ErrorViewResolver() {
+			@Override
+			public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
+				return status == HttpStatus.NOT_FOUND
+						? new ModelAndView("index.html", Collections.<String, Object>emptyMap(), HttpStatus.OK)
+						: null;
+			}
+		};
+	}
 }
